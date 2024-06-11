62e439eeb5dc8ff16248cb8707aaa4702a66778c
mohammedajmal0 <mohammedajmal007.0@gmail.com>
2024-06-11 22:32:06 +0530

    main branch initialization

---
+++ config/envLoader.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const ENV=[
+    "PORT",
+    "DB_NAME",
+    "DB_URL"
+] as const
+
+const loadVar=(env:readonly String[]):Record<string,string> =>{
+    const variables: Record<string, string> = {}
+    env.forEach(name => {
+        const value = process.env[`${name}`]
+    
+        if (value) {
+          variables[`${name}`] = value
+        } else {
+          console.error(`Env ${name} not found`)
+        }
+      })
+    
+      return variables
+}
+
+export const variables: Record<(typeof ENV)[number], string> = loadVar(ENV)
